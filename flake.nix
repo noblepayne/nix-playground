4bd9de7e9ab60b1c32aa79c22a12e9f25e98ec94
Wes Payne <noblepayne@noblepayne.com>
2023-09-08 14:37:28 -0700

    experiment 1

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "a whole big mess o' nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      "initrd" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./initrd.nix
+        ];
+      };
+    };
+  };
+}
---
+++ initrd.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      # Modules...
+    ];
+
+  networking.hostName = "bootie";
+
+  # Enable networking
+  # networking.networkmanager.enable = true;
+  networking.useDHCP = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  documentation.nixos.enable = false;
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.openssh.passwordAuthentication = true;
+
+  system.stateVersion = "unstable";
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  boot.initrd.network.enable = true;
+  boot.initrd.extraFiles = {
+    "/etc/nix/nix.conf" = {"source" = pkgs.writeTextFile {name = "nixconf"; text = "experimental-features = nix-command flakes";};};
+    "/tools/nix" = {"source" = pkgs.stdenv.mkDerivation {
+      name = "wrappedNix";
+      dontUnpack = true;
+      dontBuild = true;
+      dontConfigure = true;
+      buildInputs = [ pkgs.makeWrapper ];
+      installPhase = ''
+        mkdir -p $out/bin
+        cp -pdv ${pkgs.nix}/bin/nix $out/bin
+        wrapProgram $out/bin/nix --set USER "ROOT" --set SSL_CERT_FILE "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+      '';
+    };};
+  };
+  boot.initrd.postDeviceCommands = ''
+    /tools/nix/bin/nix run nixpkgs#htop
+  '';
+}
